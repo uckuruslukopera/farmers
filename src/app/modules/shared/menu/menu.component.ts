4ef0dceb54c8a974019b0aa8b0647d90fc5a34ec
uckuruslukopera <mrvtysz@gmail.com>
2019-05-09 19:54:04 +0300

    MG-24 add menu component

--- src/app/modules/shared/menu/menu.component.ts
+++ src/app/modules/shared/menu/menu.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
 import { CategoriesService } from 'src/app/services/categories.service';
 import { Category } from 'src/app/models/category.interface';
+
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnInit {
-
+export class MenuComponent implements OnInit, OnDestroy {
+  
+  subscription: Subscription;
   categories: Category[] = [];
 
   constructor(
@@ -16,12 +20,16 @@ export class MenuComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.categoriesService.getCategories().subscribe(
+    this.subscription = this.categoriesService.getCategories().subscribe(
       (response: Category[]) => {
         this.categories = [...response];
         console.log(this.categories);
       }, error => console.log(error)
     );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
