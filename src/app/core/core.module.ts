7123b1773ee0bb5f8e893487e3230f15c3af1f69
uckuruslukopera <mrvtysz@gmail.com>
2019-05-09 18:00:10 +0300

    Add routerLink to homepage

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, ShoppingCartComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     HeaderComponent,
